d47dfe7f1d2291edd7ca9f5a8179b07ad8609e8e
johndward01 <johndward01@yahoo.com>
2020-12-22 17:57:29 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// *** ex1 is what you will be using to manipulate the lorem ipsum text ***
+let ex1 = document.getElementById("ex1");
+
+// TODO: create a for loop that repeat 5 times and put this code inside of its scope:
+
+// ex1.insertAdjacentHTML('afterbegin', `<p>${i} Lorem ipsum dolor sit amet consectetur, adipisicing elit!</p>`);
+
+
